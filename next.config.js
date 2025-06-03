016790a58df657d3b15d78e88587a69c715ac625
SamCapela <sam.Capela@hotmail.fr>
2025-06-03 18:12:10 +0200

    sdsssgmm

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 // next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
         return [
@@ -8,15 +7,15 @@ const nextConfig = {
                 headers: [
                     {
                         key: "Content-Security-Policy",
-                        value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https:;
-              frame-src 'self' https://clips.twitch.tv;
-              connect-src 'self' https://api.twitch.tv https://gql.twitch.tv;
-              font-src 'self';
-            `.replace(/\s{2,}/g, ' ').trim(),
+                        value: [
+                            "default-src 'self';",
+                            "script-src 'self' 'unsafe-inline';",
+                            "style-src 'self' 'unsafe-inline';",
+                            "img-src 'self' data: https:;",
+                            "frame-src 'self' https://clips.twitch.tv;",
+                            "connect-src 'self' https://api.twitch.tv https://gql.twitch.tv;",
+                            "font-src 'self';",
+                        ].join(" "),
                     },
                 ],
             },
